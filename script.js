742253af1e8a83523f146888a0d9f3976c61c229
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2024-04-23 23:52:24 +0300

    .

--- script.js
+++ script.js
@@ -98,3 +98,132 @@ const calcAge = function (birthYear) {
   console.log(this);
 };
 calcAge(1991);
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+
+
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+// var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+
+
+///////////////////////////////////////
+// Objects vs. primitives
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
+
+
+///////////////////////////////////////
+// Primitives vs. Objects in Practice
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName; 
+*/
+const arr = [1, 'mimi', 2];
+
+let a = arr[0];
+let b = arr[1];
+let c = arr[2];
+ console.log(a,b,c);
+
+const arr1  = [1, 2, 3];
+let [y,z,x] = arr1;
+console.log(y,z,x); 
+
+let resturant = {
+name: 'Mariya VIKI',
+menu: ["pasta","pizza","wather","salet"],
+servi:['Iva','Stoyan','Asen','Mimi','Viki',],
+location: 'Sofia, Bulgaria',
+order: function(starIndex,starMenu){
+    return[this.menu[starIndex],this.servi[starMenu]]
+}
+}
+
+const [res1, res2] = resturant.servi;
+console.log(res1,res2);
+let [rest3, ,rest4] = resturant.servi; ///
+
+[rest3, rest4] = [rest4, rest3];
+console.log(rest3,rest4); 
+const [st1,st2]  = resturant.order(0,0);
+console.log(st1, st2);
