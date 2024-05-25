97d6e6595a7c9b395d63376d9b32908adbfffd5b
Mariya Hadzhiyska <136964468+DevMarieta@users.noreply.github.com>
2024-05-25 23:58:23 +0300

    .

--- script.js
+++ script.js
@@ -92,17 +92,15 @@
 // // console.log(starter, mainCourse); // Garlic Bread Pizza
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// //107. Rest Pattern and Parameters
-// //1) arrays
+//Rest Pattern and Parameters
+//1) arrays
 // //spread in right side 
 // const arg = [1,2, [3,4]];
 
 // //rest in left side
 // const [a, b, ... others] = [1,2,3,'Mimi','Georgy', 'Nikolay'];
 // console.log(a,b, others);
-
-// //combo
+//combo
 // const [c, , d, ...otherFood]=[... restaurants.mainMenu, ...restaurants.starterMenu]
 // console.log(c, d, otherFood);
 
@@ -115,7 +113,7 @@
 // const add = function (...numbers){
 //   let sum =0;
 //   for(let i=0; i<numbers.length; i++){
-  sum += number[1]
+  //sum += number[1]
 //     console.log(sum);
 //   }
 
@@ -124,78 +122,14 @@
 // add(3,5,5,7,6);
 // add(2,3,4,4);
 // add(3,6,22,44,5,8);
-let x = [1,2,3]
-add(...x)
-// // String Methods Practice
+//let x = [1,2,3]
+//add(...x)
 
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// // 🔴 Delayed Departure from FAO to TXL (11h25)
-// //              Arrival from BRU to FAO (11h45)
-// //   🔴 Delayed Arrival from HEL to FAO (12h05)
-// //            Departure from FAO to LIS (12h30)
-
-// const getCode = str => str.slice(0, 3).toUpperCase();
-
-// for (const flight of flights.split('+')) {
-//   const [type, from, to, time] = flight.split(';');
-//   const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
-//     '_',
-//     ' '
-//   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
-//   console.log(output);
-// }
-
-// Coding Challenge #4
-
-/* 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
-
-The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
-
-THIS TEST DATA (pasted to textarea)
-underscore_case
- first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure
-
-SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
-underscoreCase      ✅
-firstName           ✅✅
-someVariable        ✅✅✅
-calculateAge        ✅✅✅✅
-delayedDeparture    ✅✅✅✅✅
-
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
-
-Afterwards, test with your own test data!
-
-GOOD LUCK 😀
-*/
-
-/*
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
-
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
-
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
-  }
-});
-*/
-//24524
-
+//Short Circuiting (&& and ||)
+// Use any data type, return eny data type, short - circuiting(късо съединение)
+console.log(3||'Mariya');
+console.log('' || 'Mariya');
+console.log(true || 0);
+console.log(undefined || null);
+/*Късо съединение- когато единят операнд е булева стойност а втората не е,
+тя изобщо няма да бъде разгледана */
